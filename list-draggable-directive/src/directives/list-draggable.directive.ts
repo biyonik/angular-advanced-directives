443864f0632301fd731bb9038d8195b028d3297b
biyonik <ahmet.altun60@gmail.com>
2024-07-13 01:39:02 +0300

    define directive properties

--- list-draggable-directive/src/directives/list-draggable.directive.ts
+++ list-draggable-directive/src/directives/list-draggable.directive.ts
@@ -1,7 +1,32 @@
-import { Directive } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, inject, Inject, Renderer2 } from '@angular/core';
+
+const constants = {
+  class: {
+    DRAG_PLACEHOLDER: 'drag-placeholder',
+    LIST_DRAG: 'list-drag',
+    DRAGGING: 'dragging',
+    HOST_DRAG: 'host-drag'
+  }
+}
 
 @Directive({
   selector: '[listDraggable]',
   standalone: true,
 })
-export default class ListDraggableDirective {}
+export default class ListDraggableDirective {
+  #mouseDown: boolean = false;
+  #dragItem?: HTMLElement;
+  #dragPlaceholder?: HTMLElement;
+  #initialX: number = 0;
+  #initialY: number = 0;
+
+  #host: ElementRef<HTMLElement> = inject(ElementRef);
+  #renderer: Renderer2 = inject(Renderer2);
+
+
+  constructor(
+    @Inject(DOCUMENT) private readonly document: Document
+  ) { }
+
+}
